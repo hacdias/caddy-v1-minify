8be2de57aea6fc97aa1f5b27690fe64a1a01d141
Henrique Dias <hacdias@gmail.com>
2016-06-08 10:52:53 +0100

    add working test with css

--- minify.go
+++ minify.go
@@ -2,9 +2,19 @@ package minify
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
 
 	"github.com/mholt/caddy"
 	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/css"
+	"github.com/tdewolff/minify/html"
+	"github.com/tdewolff/minify/js"
+	"github.com/tdewolff/minify/json"
+	"github.com/tdewolff/minify/svg"
+	"github.com/tdewolff/minify/xml"
 )
 
 func init() {
@@ -34,6 +44,31 @@ type CaddyMinify struct {
 // ServeHTTP is the main function of the whole plugin that routes every single
 // request to its function.
 func (h CaddyMinify) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	if strings.HasSuffix(r.URL.Path, ".css") {
+
+		rec := httptest.NewRecorder()
+
+		code, err := h.Next.ServeHTTP(rec, r)
+
+		m := minify.New()
+		m.AddFunc("text/css", css.Minify)
+		m.AddFunc("text/html", html.Minify)
+		m.AddFunc("text/javascript", js.Minify)
+		m.AddFunc("image/svg+xml", svg.Minify)
+		m.AddFuncRegexp(regexp.MustCompile("[/+]json$"), json.Minify)
+		m.AddFuncRegexp(regexp.MustCompile("[/+]xml$"), xml.Minify)
+
+		b, err := m.Bytes("text/css", rec.Body.Bytes())
+		if err != nil {
+			panic(err)
+		}
+
+		w.Write(b)
+
+		//	w.Write(rec.Body.Bytes())
+
+		return code, err
+	}
 
 	return h.Next.ServeHTTP(w, r)
 }
